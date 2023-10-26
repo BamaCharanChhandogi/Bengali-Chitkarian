5f5a1f346f149590cc426a292e78a5dc5698cb1f
BamaCharanChhandogi <b.c.chhandogi@gmail.com>
2023-10-26 21:04:46 +0530

    update

--- src/Components/UserFeed.jsx
+++ src/Components/UserFeed.jsx
@@ -3,9 +3,9 @@ import "../App.css";
 import UserFeedData from "./UserFeedData";
 function UserFeed(props) {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto mt-3">
       <h2 className="text-center my-2 p-1 text-xl sm:text-2xl font-bold">Top Bengali Profiles</h2>
-      <div className="flex flex-wrap gap-y-4 gap-x-4 bg-gray-900 sm:py-8 sm:mx-4 rounded justify-center">
+      <div className="flex flex-wrap bg-gray-900 sm:py-8 sm:mx-4 px-2 rounded ">
       {props.data.map((user) => {
         const { id, data } = user;
         return (
--- src/Components/UserFeedData.jsx
+++ src/Components/UserFeedData.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
-import UserDetails from "./UserDetails";
+import React from "react";
 import { Link } from "react-router-dom";
-// import "../App.css";
+
 function UserFeedData(props) {
   return (
-    <div className=" w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4 flex flex-col bg-gray-200 rounded shadow-md cursor-pointer border-r-4 border-b-4 border-indigo-500">
-   <Link to={`/user/${props.id}`}>
-      <img
-        src={props.profileImg}
-        alt="Profile Avatar"
-        className="w-32 h-32 rounded-full mx-auto object-cover"
-      />
-      <h3>{props.firstName}</h3>
-    </Link>
+    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
+      <Link to={`/user/${props.id}`}>
+        <div className="flex flex-col items-center p-3 bg-gray-200 rounded shadow-md border-r-4 border-b-4  hover:border-pink-500 hover:cursor-pointer transition duration-300">
+          <img
+            src={props.profileImg}
+            alt="Profile Avatar"
+            className="w-32 h-32 rounded-full mx-auto object-cover"
+          />
+          <h3 className="text-lg sm:text-xl mt-2 text-center">
+            {props.firstName}
+          </h3>
+        </div>
+      </Link>
     </div>
   );
 }
