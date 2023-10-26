d1b28bfe098ec7729cfa24429e3d92033660e9bb
BamaCharanChhandogi <b.c.chhandogi@gmail.com>
2023-10-26 23:40:45 +0530

    update

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -191,7 +191,7 @@ function SignUp() {
                   </label>
                 </div>
                 <button
-                  className="w-72 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+                  className="w-full py-3 m-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
                   onClick={submitSignUp}
                 >
                   Sign Up
